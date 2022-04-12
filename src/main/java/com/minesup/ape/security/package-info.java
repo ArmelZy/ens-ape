39219d652589ee668336f581ed182f597b912563
BOBDA DJIMO ARMEL HYACINTHE <armelhbobdad@gmail.com>
2022-04-12 21:43:18 +0100

    Initial version of ape generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/minesup/ape/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.minesup.ape.security;
---
+++ src/test/java/com/minesup/ape/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.minesup.ape;
+
+import com.minesup.ape.ApeApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = ApeApp.class)
+public @interface IntegrationTest {
+}
